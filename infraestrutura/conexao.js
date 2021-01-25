6e65b83c197390bb6a9dc91fcf5dbf654bf6ec83
Lucas <lucasrochadealmeida@hotmail.com>
2021-01-24 21:05:57 -0300

    importando mysql

---
+++ infraestrutura/conexao.js
@@ -0,0 +1,11 @@
+const mysql = require('mysql2')
+
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    port: '33061',
+    user: 'root',
+    password: 'Xk2ag47HQ4lucas',
+    database: 'agenda-petshop'
+})
+
+module.exports = conexao
