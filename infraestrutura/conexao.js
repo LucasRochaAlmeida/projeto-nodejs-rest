7a47b97ab456a6ed6bf8083312688dfa14a78d76
Lucas <lucasrochadealmeida@hotmail.com>
2021-01-27 22:09:56 -0300

    outras configurações

--- infraestrutura/conexao.js
+++ infraestrutura/conexao.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2')
 
 const conexao = mysql.createConnection({
     host: 'localhost',
-    port: '33061',
+    port: 33061,
     user: 'root',
     password: 'Xk2ag47HQ4lucas',
     database: 'agenda-petshop'
