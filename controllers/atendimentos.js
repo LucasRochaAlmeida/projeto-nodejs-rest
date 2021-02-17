bf9d54eadd8261e28bcc8daf544328a4bdd557ad
Lucas <lucasrochadealmeida@hotmail.com>
2021-02-16 21:35:17 -0300

    atualizando rotas

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,13 +1,22 @@
 const Atendimento = require('../models/atendimentos')
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos') )
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res)
+    } )
+
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscaPorId(id, res)
+
+    } )
 
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body
 
-        Atendimento.adiciona(atendimento)
-        res.send('Você está na rota de atendimentos e realizando um POST')
+        Atendimento.adiciona(atendimento, res)
+        
     } )
 }
 
